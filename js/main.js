fc421a7cbf43440ac152e4cdbd39fb0cbd835045
nullTony <vnullsoul@mail.com>
2026-02-20 17:43:39 +0500

    add header and navbar

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const header = document.getElementById('header');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
